64551b2181573959d4e7bc9643cccb9753b440d7
Shannon Craig <shannoncraig92@outlook.com>
2019-11-03 13:31:49 +0000

    Basic collection of object done

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public GameObject player = null;
+    private bool _isCollected = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosition = new Vector3(player.transform.position.x, player.transform.position.y + 1.0f, player.transform.position.z + 1.0f);
+       
+        if (_isCollected)
+        {
+            transform.position = playerPosition;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isCollected = true;
+        }
+
+        else if (other.tag == "Bin")
+        {
+            // add points to player's score
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _mSpeed = 5.0f;
+    private float _movementSpeed = 5.0f;
+    [SerializeField]
+    private int _playerScore = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,6 @@ public class Player : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
 
-        transform.Translate(direction * _mSpeed * Time.deltaTime);
+        transform.Translate(direction * _movementSpeed * Time.deltaTime);
     }
 }
