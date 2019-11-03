91af6d252aadf57b8f0956d8a596ff89fd85d15e
Shannon Craig <shannoncraig92@outlook.com>
2019-11-03 22:10:55 +0000

    Objects spawning with random velocity

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -14,6 +14,9 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _plasticObjectPrefab;
     int random = 0;
+    private Vector3 _direction;
+    [SerializeField]
+    private float _speed = 20.0f;
 
 
     // Start is called before the first frame update
@@ -25,7 +28,7 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        _direction = transform.forward + (transform.rotation * new Vector3(0, 20, 10) * Random.Range(0, 1));
     }
 
     IEnumerator SpawnRoutine()
@@ -37,29 +40,40 @@ public class SpawnManager : MonoBehaviour
             switch (random)
             {
                 case 1:
-                    Vector3 positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
-                    Instantiate(_canObjectPrefab, positionToSpawn, Quaternion.identity);
+                    //Vector3 positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    Vector3 positionToSpawn = new Vector3(0.0f, 8.0f, 0.0f);
+                    GameObject canObj = (GameObject)Instantiate(_canObjectPrefab, positionToSpawn, transform.rotation);
+                    canObj.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
                     yield return new WaitForSeconds(5.0f);
                     break;
 
                 case 2:
-                    positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
-                    Instantiate(_glassObjectPrefab, positionToSpawn, Quaternion.identity);
+                    //positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    positionToSpawn = new Vector3(0.0f, 8.0f, 0.0f);
+                    GameObject glassObj = (GameObject)Instantiate(_glassObjectPrefab, positionToSpawn, transform.rotation);
+                    glassObj.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
                     yield return new WaitForSeconds(5.0f);
                     break;
 
                 case 3:
-                    positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
-                    Instantiate(_paperObjectPrefab, positionToSpawn, Quaternion.identity);
+                    // positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    positionToSpawn = new Vector3(0.0f, 8.0f, 0.0f);
+                    GameObject paperObj = (GameObject)Instantiate(_paperObjectPrefab, positionToSpawn, transform.rotation);
+                    paperObj.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
                     yield return new WaitForSeconds(5.0f);
                     break;
 
                 case 4:
-                    positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
-                    Instantiate(_plasticObjectPrefab, positionToSpawn, Quaternion.identity);
+                    //positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    positionToSpawn = new Vector3(0.0f, 8.0f, 0.0f);
+                    GameObject plasticObj = (GameObject)Instantiate(_plasticObjectPrefab, positionToSpawn, transform.rotation);
+                    plasticObj.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
                     yield return new WaitForSeconds(5.0f);
                     break;
             }
+
+            //_canObjectPrefab.GetComponent<Rigidbody>().velocity = _direction * _speed * Time.deltaTime;
+            //yield return new WaitForSeconds(5.0f);
         }
     }
 }
