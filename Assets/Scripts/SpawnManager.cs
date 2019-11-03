6ac714dd11da5cba43155e8fd19a36d854bb2341
Shannon Craig <shannoncraig92@outlook.com>
2019-11-03 21:49:44 +0000

    Objects randomly falling from above

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _canObjectPrefab;
+    [SerializeField]
+    private GameObject _glassObjectPrefab;
+    [SerializeField]
+    private GameObject _paperObjectPrefab;
+    [SerializeField]
+    private GameObject _plasticObjectPrefab;
+    int random = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            random = Random.Range(1, 4);
+
+            switch (random)
+            {
+                case 1:
+                    Vector3 positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    Instantiate(_canObjectPrefab, positionToSpawn, Quaternion.identity);
+                    yield return new WaitForSeconds(5.0f);
+                    break;
+
+                case 2:
+                    positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    Instantiate(_glassObjectPrefab, positionToSpawn, Quaternion.identity);
+                    yield return new WaitForSeconds(5.0f);
+                    break;
+
+                case 3:
+                    positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    Instantiate(_paperObjectPrefab, positionToSpawn, Quaternion.identity);
+                    yield return new WaitForSeconds(5.0f);
+                    break;
+
+                case 4:
+                    positionToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, Random.Range(-8.0f, 8.0f));
+                    Instantiate(_plasticObjectPrefab, positionToSpawn, Quaternion.identity);
+                    yield return new WaitForSeconds(5.0f);
+                    break;
+            }
+        }
+    }
+}
